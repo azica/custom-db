bc5a44e3c6d3a86853b25de5a8eceed6ba12ef05
Aizada83 <a.samudinova@designingit.com>
2023-08-19 15:10:38 +0600

    fix

--- api/server.js
+++ api/server.js
@@ -1,19 +1,32 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const server = jsonServer.create()
+const auth = require('json-server-auth')
+
+const app = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
+// /!\ Bind the router db to the app
+app.db = router.db
+
+
+
+app.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
+const rules = auth.rewriter({
+	// Permission rules
+	users: 600,
+	messages: 640,
+	// Other rules
+	'/posts/:category': '/posts?category=:category',
+  })
+
+app.use(rules)
+app.use(auth)
+app.use(router)
+
+app.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
 // Export the Server API
-module.exports = server
+module.exports = app
